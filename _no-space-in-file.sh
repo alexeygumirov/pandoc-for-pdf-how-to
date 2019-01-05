2ddacc2b631f95270c9d51c495f3348617125d61
Alexey Gumirov <ag_devops@die-optimisten.net>
2019-01-05 18:30:43 +0100

    script update

--- _no-space-in-file.sh
+++ _no-space-in-file.sh
@@ -3,24 +3,24 @@
 # This script checks names of the files in the target folder
 # and replaces them with "_" (underscore) symbol.
 
-# Input parameter to the script is a path to directory
+# Input parameter to the script is a path to the target folder
 
 SOURCE=$1
 INITDIR=$PWD
 
-if [[ ! -e "$SOURCE" ]]; # First check if the target does exist in the file system
+if [[ ! -e $SOURCE ]]; # First check if the target does exist in the file system
 then
-	echo "Target does not exist"
+	echo "Target does not exist."
 	exit 1
 fi
 
 if [[ -d $SOURCE ]]; then # Checking if target is a directory.
-	cd $SOURCE
+	cd "$SOURCE"
 	for file in *
 	do
 		rename 's/ /_/g' "$file"
 	done
-	cd $INITDIR	
+	cd "$INITDIR"	
 	exit 0
 elif [[ ! -d $SOURCE ]]; then # Checking, if target is not a directory (e.g. file, device, etc.)
 	echo "Error! Please enter directory as input."
