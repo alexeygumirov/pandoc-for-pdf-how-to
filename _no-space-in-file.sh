fa34aded197248d4bc817fea7a32d6830cc24310
Alexey Gumirov <ag_devops@die-optimisten.net>
2019-01-15 14:38:23 +0100

    _no-space-in-file.sh script update

--- _no-space-in-file.sh
+++ _no-space-in-file.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+
 # This script checks names of the files in the target folder
 # and replaces them with "_" (underscore) symbol.
 
@@ -7,22 +7,29 @@
 
 SOURCE=$1
 INITDIR=$PWD
+FILE_LIST="/tmp/find_list"
 
-if [[ ! -e $SOURCE ]]; # First check if the target does exist in the file system
+if [[ ! -e "$SOURCE" ]]; # First check if the target does exist in the file system
 then
 	echo "Target does not exist."
 	exit 1
 fi
 
-if [[ -d $SOURCE ]]; then # Checking if target is a directory.
+if [[ -d $SOURCE ]]; # Checking if target is a directory
+then
 	cd "$SOURCE"
-	for FILE in $(find . -maxdepth 1 -type f -name "*" -printf "%f\n")
+	find . -maxdepth 1 -type f -name "*" -printf %f"\n" > "$FILE_LIST"
+	while read -r FILE
 	do
-		rename 's/ /_/g' "$FILE"
-	done
-	cd "$INITDIR"	
+		if [[ ! -z $FILE ]]; then
+			rename 's/ /_/g' "$FILE"
+		fi
+	done < "$FILE_LIST"
+	rm "$FILE_LIST"
+	cd "$INITDIR"
 	exit 0
-elif [[ ! -d $SOURCE ]]; then # Checking, if target is not a directory (e.g. file, device, etc.)
+elif [[ ! -d $SOURCE ]]; 
+then
 	echo "Error! Please enter directory as input."
 	exit 1
-fi
+fi	
