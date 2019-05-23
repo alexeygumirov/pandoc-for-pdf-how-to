f7b439806a1757824f927b152e477233ebf5f669
Alexey Gumirov <ag_devops@die-optimisten.net>
2019-05-24 00:02:13 +0200

    Script is updated for Pandoc 2.7, Eisvogel_mod template is fixed.

--- _pdf-gen.sh
+++ _pdf-gen.sh
@@ -6,10 +6,10 @@
 SOURCE_FILE_NAME="README"
 DEST_FILE_NAME="pandoc-2-pdf-how-to"
 YAML_FILE="_yaml-block.yaml"
-TEMPLATE="eisvogel_mod"
+TEMPLATE="eisvogel_mod.latex"
 DATE=$(date "+%d %B %Y")
 # SOURCE_FORMAT="markdown_github+yaml_metadata_block+smart+implicit_figures"
-SOURCE_FORMAT="markdown_github+yaml_metadata_block+implicit_figures+table_captions+footnotes"
+SOURCE_FORMAT="markdown_github+yaml_metadata_block+implicit_figures+table_captions+footnotes+smart"
 
-pandoc -s -S -o "$DEST_FILE_NAME".pdf -f "$SOURCE_FORMAT" --template "$TEMPLATE" --toc --listings --number-section --dpi=300 -M date="$DATE" -V lang=en-US "$YAML_FILE" "$SOURCE_FILE_NAME".md >&1
+pandoc -s -o "$DEST_FILE_NAME".pdf -f "$SOURCE_FORMAT" --template "$TEMPLATE" --toc --listings --number-section --dpi=300 --pdf-engine xelatex -M date="$DATE" -V lang=en-US "$YAML_FILE" "$SOURCE_FILE_NAME".md >&1
 # pandoc -s -o "$DEST_FILE_NAME".pdf -f "$SOURCE_FORMAT" --template "$TEMPLATE" --toc --listings ---number-sectio n-dpi=300 -M date="$DATE" -V lang=en-US "$YAML_FILE" "$SOURCE_FILE_NAME".md >&1
