4abbc1294b6438f23bf370774b5206e742ec4a3f
Alexey Gumirov <ag_devops@die-optimisten.net>
2019-05-24 08:11:03 +0200

    Script is made for Pandoc 1.x and Pandoc 2.x

--- _pdf-gen.sh
+++ _pdf-gen.sh
@@ -8,8 +8,14 @@ DEST_FILE_NAME="pandoc-2-pdf-how-to"
 YAML_FILE="_yaml-block.yaml"
 TEMPLATE="eisvogel_mod.latex"
 DATE=$(date "+%d %B %Y")
+
+# For Pandoc 1.x
 # SOURCE_FORMAT="markdown_github+yaml_metadata_block+smart+implicit_figures"
+#
+# pandoc -s -S -o "$DEST_FILE_NAME".pdf -f "$SOURCE_FORMAT" --template "$TEMPLATE" --toc --listings --number-section --dpi=300 --pdf-engine xelatex -M date="$DATE" -V lang=en-US "$YAML_FILE" "$SOURCE_FILE_NAME".md >&1
+
+
+# For Pandoc 2.x
 SOURCE_FORMAT="markdown_github+yaml_metadata_block+implicit_figures+table_captions+footnotes+smart"
 
 pandoc -s -o "$DEST_FILE_NAME".pdf -f "$SOURCE_FORMAT" --template "$TEMPLATE" --toc --listings --number-section --dpi=300 --pdf-engine xelatex -M date="$DATE" -V lang=en-US "$YAML_FILE" "$SOURCE_FILE_NAME".md >&1
-# pandoc -s -o "$DEST_FILE_NAME".pdf -f "$SOURCE_FORMAT" --template "$TEMPLATE" --toc --listings ---number-sectio n-dpi=300 -M date="$DATE" -V lang=en-US "$YAML_FILE" "$SOURCE_FILE_NAME".md >&1
