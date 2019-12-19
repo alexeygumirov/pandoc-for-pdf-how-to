819ca292e95bb77350e6b87c5096bea3d595bcba
Alexey Gumirov <ag_devops@die-optimisten.net>
2019-12-19 12:48:52 +0100

    protected PDF chapter is added

--- _pdf-gen.sh
+++ _pdf-gen.sh
@@ -4,17 +4,16 @@
 # You shall install pandoc and texlive packages to make it work
 
 SOURCE_FILE_NAME="README"
-DEST_FILE_NAME="pandoc-2-pdf-how-to"
+DEST_FILE_NAME="pandoc-2-pdf-how-to.pdf"
+DEST_FILE_NAME_PROTECTED="pandoc-2-pdf-how-to_(protected).pdf"
 INDEX_FILE="INDEX"
 TEMPLATE="eisvogel_mod.latex"
 DATE=$(date "+%d %B %Y")
+DATA_DIR="pandoc"
 
-# For Pandoc 1.x
-#
-# SOURCE_FORMAT="markdown_github+yaml_metadata_block+smart+implicit_figures"
-# pandoc -s -S -o "$DEST_FILE_NAME".pdf -f "$SOURCE_FORMAT" --template "$TEMPLATE" --toc --listings --number-section --dpi=300 --pdf-engine xelatex -M date="$DATE" -V lang=en-US $(cat "$INDEX_FILE") >&1
-
-# For Pandoc 2.x
-#
 SOURCE_FORMAT="markdown_github+yaml_metadata_block+implicit_figures+table_captions+footnotes+smart"
-pandoc -s -o "$DEST_FILE_NAME".pdf -f "$SOURCE_FORMAT" --template "$TEMPLATE" --toc --listings --number-section -V subparagraph --dpi=300 --pdf-engine xelatex -M date="$DATE" -V lang=en-US $(cat "$INDEX_FILE") >&1
+pandoc -s -o "$DEST_FILE_NAME" -f "$SOURCE_FORMAT" --data-dir="$DATA_DIR" --template "$TEMPLATE" --toc --listings --number-section -V subparagraph --dpi=300 --pdf-engine xelatex -M date="$DATE" -V lang=en-US $(cat "$INDEX_FILE") >&1
+
+OWNER_PASSWORD=$(date | md5sum | cut -d ' ' -f 1)
+
+qpdf --object-streams=disable --encrypt "" "$OWNER_PASSWORD" 256 --print=none --modify=none --extract=n -- "$DEST_FILE_NAME" "$DEST_FILE_NAME_PROTECTED"
