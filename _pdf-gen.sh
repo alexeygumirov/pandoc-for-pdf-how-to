86e0148c0ccdee07a988bf67062a3da60c92c6f0
Alexey Gumirov <ag_devops@die-optimisten.net>
2021-01-11 14:00:06 +0100

    auto_identifiers are added

--- _pdf-gen.sh
+++ _pdf-gen.sh
@@ -14,11 +14,13 @@ DATA_DIR="pandoc"
 SOURCE_FORMAT="markdown_strict\
 +backtick_code_blocks\
 +pipe_tables\
++auto_identifiers\
 +yaml_metadata_block\
 +implicit_figures\
 +table_captions\
 +footnotes\
 +smart\
++escaped_line_breaks\
 +header_attributes"
 
 pandoc -s -o "$DEST_FILE_NAME" -f "$SOURCE_FORMAT" --data-dir="$DATA_DIR" --template "$TEMPLATE" --toc --listings --columns=50 --number-section --dpi=300 --pdf-engine xelatex -M date="$DATE" $(cat "$INDEX_FILE") >&1
